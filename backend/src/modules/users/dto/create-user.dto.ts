65a583a78691772ee736915d21e96207131a8789
anjaranirina-omega <anjaranirinaomega@gmail.com>
2026-07-10 14:11:00 +0300

    Ajout du socle utilisateurs et rôles

---
+++ backend/src/modules/users/dto/create-user.dto.ts
@@ -0,0 +1,34 @@
+import {
+  IsBoolean,
+  IsEmail,
+  IsOptional,
+  IsString,
+  MinLength,
+} from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  firstName!: string;
+
+  @IsString()
+  lastName!: string;
+
+  @IsEmail()
+  email!: string;
+
+  @IsString()
+  @MinLength(6)
+  password!: string;
+
+  @IsOptional()
+  @IsString()
+  phone?: string;
+
+  @IsOptional()
+  @IsString()
+  roleId?: string;
+
+  @IsOptional()
+  @IsBoolean()
+  isActive?: boolean;
+}
