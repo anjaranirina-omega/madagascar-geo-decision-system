fe0436f3e859896ccb6e42171731667c9cfcab3d
anjaranirina-omega <anjaranirinaomega@gmail.com>
2026-07-30 10:39:20 +0300

    Ajout des indicateurs zonaux du risque inondation

--- backend/src/modules/zone-indicators/zone-indicators.module.ts
+++ backend/src/modules/zone-indicators/zone-indicators.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ZoneIndicator } from './entities/zone-indicator.entity';
+import { ZoneRiskIndicator } from './entities/zone-risk-indicator.entity';
 import { ZoneIndicatorsController } from './zone-indicators.controller';
 import { ZoneIndicatorsService } from './zone-indicators.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ZoneIndicator])],
+  imports: [TypeOrmModule.forFeature([ZoneIndicator, ZoneRiskIndicator])],
   controllers: [ZoneIndicatorsController],
   providers: [ZoneIndicatorsService],
   exports: [ZoneIndicatorsService],
